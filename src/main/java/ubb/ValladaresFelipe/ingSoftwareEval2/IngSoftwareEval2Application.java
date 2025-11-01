b1b3a76a2a8b0c30ff86b25fd8f26a398ed36164
FValladares2 <116167241+FValladares2@users.noreply.github.com>
2025-11-01 14:26:20 -0300

    Archivos base springboot

---
+++ src/main/java/ubb/ValladaresFelipe/ingSoftwareEval2/IngSoftwareEval2Application.java
@@ -0,0 +1,13 @@
+package ubb.ValladaresFelipe.ingSoftwareEval2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSoftwareEval2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngSoftwareEval2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ubb/ValladaresFelipe/ingSoftwareEval2/IngSoftwareEval2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ubb.ValladaresFelipe.ingSoftwareEval2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngSoftwareEval2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
